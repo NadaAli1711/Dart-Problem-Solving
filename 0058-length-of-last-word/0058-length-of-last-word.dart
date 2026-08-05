51d1870fe0f1481c022abd4ec9c0f2c4b9416295
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-05 19:32:13 +0300

    added four approaches for solving the problem

--- 0058-length-of-last-word/0058-length-of-last-word.dart
+++ 0058-length-of-last-word/0058-length-of-last-word.dart
@@ -1,3 +1,6 @@
+// Approach 1:
+  // ---------------------------------------------------------------------------
+
 class Solution {
   int lengthOfLastWord(String s) {
     int count = 0;
@@ -9,4 +12,40 @@ class Solution {
     return count;
   
   }
+}
+// Approach 2: 
+  // ---------------------------------------------------------------------------
+class Solution {
+  int lengthOfLastWord(String s) {
+    final words = s.trim().split(' ');
+    return words.last.length;
+  
+  }
+}
+
+// Approach 3: 
+  // ---------------------------------------------------------------------------
+class Solution {
+  int lengthOfLastWord(String s) {
+  List<String> splited = s.trim().split(' ');
+  String lastWord = splited[splited.length - 1];
+    return lastWord.length;
+  }
+}
+
+// Approach 4: 
+  // ---------------------------------------------------------------------------
+class Solution {
+  int lengthOfLastWord(String s) {
+   String trimmedString = s.trim();
+   int count = 0;
+    int i = trimmedString.length - 1;
+    while(trimmedString[i] != ' '){
+        count++;
+      if(i !=0){
+        i--;
+      }
+    }
+    return count ;
+}
 }
