93944a0503c4efd5538e922538eaad39652280b4
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-06 14:56:21 +0300

    Add comments for int conversion in reverseString
    Added comments explaining the conversion of double to int.

--- 0344-reverse-string/0344-reverse-string.dart
+++ 0344-reverse-string/0344-reverse-string.dart
@@ -1,5 +1,7 @@
 class Solution {
   void reverseString(List<String> s) {
+    // we could use this way to convert double into int 
+    //     int mid = (s.length / 2).toInt();
     int mid = s.length ~/ 2;
     for(int i = 0 ; i < mid ; i++){
         String temp = s[i];
