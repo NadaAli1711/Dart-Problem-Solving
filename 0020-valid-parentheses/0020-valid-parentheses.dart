632776b086d86e22a8e349a5de61325d38a0b168
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-06 15:30:21 +0300

    0020-valid-parentheses 08-06-2026 Easy .dart 4 ms (68.16%) 146.1 MB (96.41%)

---
+++ 0020-valid-parentheses/0020-valid-parentheses.dart
@@ -0,0 +1,30 @@
+class Solution {
+  bool isValid(String s) {
+    List<String> stack = [];
+    Map<String,String> map = {
+    '{':'}',
+    '(':')',
+    '[':']'
+    };
+    bool stackUsedFlag = false;
+    int i = 0;
+    for( ; i < s.length ; i++){
+        if(s[i] != '}' && s[i] != ')' && s[i] != ']' ){
+            stack.add(s[i]);
+            stackUsedFlag = true;
+        }
+        else{
+            if(i < s.length){
+            if(!stack.isEmpty){
+                if(map[stack.last] != s[i]) return false;
+                stack.removeLast();
+            }else return false;
+            }
+
+        }
+    }
+    if(stack.isEmpty&& stackUsedFlag) return true;
+    return false;
+    
+  }
+}
