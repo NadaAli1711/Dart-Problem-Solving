e58dcd2ff1631603e0b576e93e1aca6fdf595b55
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-07 16:46:43 +0000

    style: auto-format Dart code

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
@@ -2,11 +2,10 @@ class Solution {
   int maxProfit(List<int> prices) {
     int minPrice = prices[0];
     int maxProfit = 0;
-    for(int i = 1 ; i < prices.length ; i++ ){
-        maxProfit = max(maxProfit, prices[i] - minPrice);
-        minPrice = min(minPrice,prices[i]);
+    for (int i = 1; i < prices.length; i++) {
+      maxProfit = max(maxProfit, prices[i] - minPrice);
+      minPrice = min(minPrice, prices[i]);
     }
     return maxProfit;
-    
   }
 }
