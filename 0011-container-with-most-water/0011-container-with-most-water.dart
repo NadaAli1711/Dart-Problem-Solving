edb58f160c20c9ac4d6e193cab344afcf4e863ce
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-07 15:58:16 +0000

    style: auto-format Dart code

--- 0011-container-with-most-water/0011-container-with-most-water.dart
+++ 0011-container-with-most-water/0011-container-with-most-water.dart
@@ -3,14 +3,14 @@ class Solution {
     int left = 0;
     int right = height.length - 1;
     int maxArea = 0;
-    while(left != right){
-        int temp = min(height[left], height[right]) * (right - left);
-        maxArea = max(maxArea,temp);
-        if(height[left] < height[right]){
-            left++;
-        }else right--;
+    while (left != right) {
+      int temp = min(height[left], height[right]) * (right - left);
+      maxArea = max(maxArea, temp);
+      if (height[left] < height[right]) {
+        left++;
+      } else
+        right--;
     }
-    return maxArea;    
+    return maxArea;
   }
-
 }
