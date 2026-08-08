df10a222c04a3cb428b9575144e891738af78d42
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-08 14:33:51 +0000

    style: auto-format Dart code

--- 0049-group-anagrams/0049-group-anagrams.dart
+++ 0049-group-anagrams/0049-group-anagrams.dart
@@ -1,17 +1,15 @@
 class Solution {
   List<List<String>> groupAnagrams(List<String> strs) {
-    Map<String , List<String>> map = {};
-    for(int i = 0 ; i < strs.length ; i++){
-        List<String> letters = strs[i].split('');
-        letters.sort();
-        String sortedString = letters.join('');
-        if(map.containsKey(sortedString)){
+    Map<String, List<String>> map = {};
+    for (int i = 0; i < strs.length; i++) {
+      List<String> letters = strs[i].split('');
+      letters.sort();
+      String sortedString = letters.join('');
+      if (map.containsKey(sortedString)) {
         map[sortedString]!.add(strs[i]);
-        }else{
-            map[sortedString] = [strs[i]];
-
-        }
-
+      } else {
+        map[sortedString] = [strs[i]];
+      }
     }
     return map.values.toList();
   }
