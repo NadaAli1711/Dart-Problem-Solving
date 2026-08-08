a0b546ffb39c22c42ff4a56f4014da47d226af2c
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-08 15:05:07 +0000

    style: auto-format Dart code

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.dart
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.dart
@@ -1,23 +1,20 @@
 class Solution {
   int longestConsecutive(List<int> nums) {
     int length = nums.length;
-    if(length <= 1) return length;
+    if (length <= 1) return length;
     nums.sort();
     int longestConsecutive = 1;
     int counter = 1;
-    for(int i = 1 ; i < length ; i++){
-        if(nums[i] == nums[i - 1] + 1){
-            counter++;
-            longestConsecutive = max(longestConsecutive , counter);
-        }else if(nums[i] == nums[i - 1]){
-            continue;
-        }else{
-            counter = 1;
-        }
-        
+    for (int i = 1; i < length; i++) {
+      if (nums[i] == nums[i - 1] + 1) {
+        counter++;
+        longestConsecutive = max(longestConsecutive, counter);
+      } else if (nums[i] == nums[i - 1]) {
+        continue;
+      } else {
+        counter = 1;
+      }
     }
     return longestConsecutive;
-
-    
   }
 }
