2d24be8bd3516d400cb7f81f286ea8a972df9a0a
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-13 14:29:15 +0300

    0724-find-pivot-index 08-13-2026 Easy .dart 0 ms (100%) 147.6 MB (100%)

---
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int pivotIndex(List<int> nums) {
+    int totalSum = 0;
+    int leftSum = 0;
+    for(int i = 0 ; i < nums.length ; i++){
+        totalSum += nums[i];
+    }
+    for(int i = 0 ; i < nums.length ; i++){
+        int rightSum = totalSum - nums[i] - leftSum;
+        if(rightSum == leftSum ) return i;
+        leftSum += nums[i];
+    }
+    return -1;
+
+  }
+}
