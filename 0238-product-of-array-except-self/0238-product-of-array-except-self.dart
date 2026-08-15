36ffdf041e8dee78ee3b018626d38a6232108dfe
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-15 19:25:44 +0300

    0238-product-of-array-except-self 08-15-2026 Medium .dart 6 ms (81.82%) 169.8 MB (32.87%)

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.dart
@@ -0,0 +1,19 @@
+class Solution {
+  List<int> productExceptSelf(List<int> nums) {
+    List<int> answer = [];
+    answer.add(1);
+    int temp = 1;
+   // [1,1,2,6]
+
+    for(int i = 1 ; i < nums.length ; i++){
+        answer.add(nums[i - 1] * answer[i - 1]);
+    }
+   // [,,8,6]
+    for(int i = nums.length - 2 ; i >= 0 ; i--){
+        temp *= nums[i + 1];
+        answer[i] *= temp; 
+    }
+    return answer;
+    
+  }
+}
