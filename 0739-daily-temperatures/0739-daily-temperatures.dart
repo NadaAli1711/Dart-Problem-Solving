0f722497719ffc853a59e3d51914268d52341d29
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-15 17:17:38 +0000

    style: auto-format Dart code

--- 0739-daily-temperatures/0739-daily-temperatures.dart
+++ 0739-daily-temperatures/0739-daily-temperatures.dart
@@ -1,14 +1,13 @@
 class Solution {
   List<int> dailyTemperatures(List<int> temperatures) {
     List<int> stack = [];
-    List<int> answer = List<int>.filled(temperatures.length,0);
-    for(int i = 0 ; i < temperatures.length ; i++){
-        while(!stack.isEmpty && temperatures[i] > temperatures[stack.last]){
-            answer[stack.last] = i - stack.last;
-            stack.removeLast();
-        }
-        stack.add(i);
-        
+    List<int> answer = List<int>.filled(temperatures.length, 0);
+    for (int i = 0; i < temperatures.length; i++) {
+      while (!stack.isEmpty && temperatures[i] > temperatures[stack.last]) {
+        answer[stack.last] = i - stack.last;
+        stack.removeLast();
+      }
+      stack.add(i);
     }
     return answer;
   }
