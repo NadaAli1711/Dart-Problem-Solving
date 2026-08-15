9b6025b234a7d695ce1f0ded2a3beeb546528fe0
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-15 16:26:06 +0000

    style: auto-format Dart code

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.dart
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.dart
@@ -3,17 +3,16 @@ class Solution {
     List<int> answer = [];
     answer.add(1);
     int temp = 1;
-   // [1,1,2,6]
+    // [1,1,2,6]
 
-    for(int i = 1 ; i < nums.length ; i++){
-        answer.add(nums[i - 1] * answer[i - 1]);
+    for (int i = 1; i < nums.length; i++) {
+      answer.add(nums[i - 1] * answer[i - 1]);
     }
-   // [,,8,6]
-    for(int i = nums.length - 2 ; i >= 0 ; i--){
-        temp *= nums[i + 1];
-        answer[i] *= temp; 
+    // [,,8,6]
+    for (int i = nums.length - 2; i >= 0; i--) {
+      temp *= nums[i + 1];
+      answer[i] *= temp;
     }
     return answer;
-    
   }
 }
