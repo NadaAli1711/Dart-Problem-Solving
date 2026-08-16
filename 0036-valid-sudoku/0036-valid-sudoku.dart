fee525cd98b841be20522a575f2390ab8d866e8f
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-16 12:19:31 +0000

    style: auto-format Dart code

--- 0036-valid-sudoku/0036-valid-sudoku.dart
+++ 0036-valid-sudoku/0036-valid-sudoku.dart
@@ -1,19 +1,21 @@
 class Solution {
   bool isValidSudoku(List<List<String>> board) {
-    List<Set<String>> rows = List<Set<String>>.generate(9,(_)=>{});
-    List<Set<String>> cols = List<Set<String>>.generate(9,(_)=>{});
-    List<Set<String>> boxes = List<Set<String>>.generate(9,(_)=>{});
-    for(int i = 0 ; i < 9 ; i ++){
-        for(int j =  0; j < 9 ; j++){
-            String currentChar = board[i][j];
-            int boxIndex = (i ~/ 3) * 3 + (j ~/ 3);
-            if(currentChar == '.') continue;
-            if(rows[i].contains(currentChar)|| cols[j].contains(currentChar) || boxes[boxIndex].contains(currentChar)) return false;
-            rows[i].add(currentChar);
-            cols[j].add(currentChar);
-            boxes[boxIndex].add(currentChar);
-            
-        }
+    List<Set<String>> rows = List<Set<String>>.generate(9, (_) => {});
+    List<Set<String>> cols = List<Set<String>>.generate(9, (_) => {});
+    List<Set<String>> boxes = List<Set<String>>.generate(9, (_) => {});
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        String currentChar = board[i][j];
+        int boxIndex = (i ~/ 3) * 3 + (j ~/ 3);
+        if (currentChar == '.') continue;
+        if (rows[i].contains(currentChar) ||
+            cols[j].contains(currentChar) ||
+            boxes[boxIndex].contains(currentChar))
+          return false;
+        rows[i].add(currentChar);
+        cols[j].add(currentChar);
+        boxes[boxIndex].add(currentChar);
+      }
     }
     return true;
   }
