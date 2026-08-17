7483298525046792778a118a9a0b13b534fc7ae7
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-17 19:37:59 +0300

    0853-car-fleet 08-17-2026 Medium .dart 176 ms (50%) 219.1 MB (50%)

---
+++ 0853-car-fleet/0853-car-fleet.dart
@@ -0,0 +1,23 @@
+class Solution {
+  int carFleet(int target, List<int> position, List<int> speed) {
+    if(position.length == 0) return 0;
+    if(position.length == 1) return 1;
+    int fleetSum = 1;
+    List<double> stack = [];
+    Map<int,int> map = {};
+    for(int i = 0 ; i < position.length ; i++){
+        map[position[i]] = speed[i];
+    }
+    position.sort((a,b)=>b.compareTo(a));
+    for(int i = 0 ; i < position.length ; i++){
+        double  currentTime = (target - position[i]) / map[position[i]]!;
+            if(!stack.isEmpty && currentTime > stack.first ){
+                fleetSum++;
+                stack.clear();
+            }
+        stack.add(currentTime);
+        }
+    
+    return fleetSum;
+  }
+}
