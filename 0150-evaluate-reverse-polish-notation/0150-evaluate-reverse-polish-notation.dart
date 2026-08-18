1255eaa23b64cdb6b35f60812f066c8f20559adc
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-18 02:10:27 +0000

    style: auto-format Dart code

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.dart
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.dart
@@ -1,27 +1,26 @@
 class Solution {
   int evalRPN(List<String> tokens) {
     List<int> stack = [];
-    for(int i = 0 ; i < tokens.length ; i++){
-        if(tokens[i] != '+' && tokens[i] != '-' && tokens[i] != '*' &&tokens[i] != '/' ){
-            stack.add(int.parse(tokens[i]));
-        }else{
-            int secondNum = stack.removeLast();
-            int firstNum = stack.removeLast();
-            if(tokens[i] == '+' ){
-                stack.add(firstNum + secondNum);
-                }
-            else if(tokens[i] == '-' ){
-                stack.add(firstNum - secondNum);
-                }
-            else if(tokens[i] == '*' ){
-                stack.add(firstNum * secondNum);
-                }
-            else if(tokens[i] == '/' ){
-                stack.add(firstNum ~/ secondNum);
-                }
+    for (int i = 0; i < tokens.length; i++) {
+      if (tokens[i] != '+' &&
+          tokens[i] != '-' &&
+          tokens[i] != '*' &&
+          tokens[i] != '/') {
+        stack.add(int.parse(tokens[i]));
+      } else {
+        int secondNum = stack.removeLast();
+        int firstNum = stack.removeLast();
+        if (tokens[i] == '+') {
+          stack.add(firstNum + secondNum);
+        } else if (tokens[i] == '-') {
+          stack.add(firstNum - secondNum);
+        } else if (tokens[i] == '*') {
+          stack.add(firstNum * secondNum);
+        } else if (tokens[i] == '/') {
+          stack.add(firstNum ~/ secondNum);
         }
+      }
     }
     return stack.removeLast();
-    
   }
 }
