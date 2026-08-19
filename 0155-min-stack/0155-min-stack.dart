34e46518988121be1395e205e680a83bf4dc9e16
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-19 20:56:35 +0300

    0155-min-stack 08-19-2026 Medium .dart 26 ms (36.96%) 226.7 MB (65.22%)

---
+++ 0155-min-stack/0155-min-stack.dart
@@ -0,0 +1,34 @@
+class MinStack {
+    final List<int> stack = [];
+    final List<int> minStack = [];
+
+  MinStack() {
+  }
+  
+  void push(int value) {
+    stack.add(value);
+    if(minStack.isEmpty || value <= minStack.last) minStack.add(value);
+  }
+  
+  void pop() {
+    int val = stack.removeLast();
+    if(minStack.last == val) minStack.removeLast();
+  }
+  
+  int top() {
+    return stack.last;
+  }
+  
+  int getMin() {
+    return minStack.last;
+  }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param3 = obj.top();
+ * int param4 = obj.getMin();
+ */
