58a049457e8bdc6d7796f257d037210a89e7c313
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-20 13:23:29 +0000

    style: auto-format Dart code

--- 0206-reverse-linked-list/0206-reverse-linked-list.dart
+++ 0206-reverse-linked-list/0206-reverse-linked-list.dart
@@ -9,14 +9,13 @@
 class Solution {
   ListNode? reverseList(ListNode? head) {
     ListNode? prev = null;
-    
-    while(head != null ){
-        ListNode? temp = head.next;
-        head.next = prev;
-        prev = head;
-        head = temp;
+
+    while (head != null) {
+      ListNode? temp = head.next;
+      head.next = prev;
+      prev = head;
+      head = temp;
     }
     return prev;
-    
   }
 }
