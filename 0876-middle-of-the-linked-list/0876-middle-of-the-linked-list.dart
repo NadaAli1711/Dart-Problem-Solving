5923b11b0d89388b34450a10747543f83697240a
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-22 13:52:50 +0300

    0876-middle-of-the-linked-list 08-22-2026 Easy .dart 0 ms (100%) 148.5 MB (77.78%)

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.dart
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? middleNode(ListNode? head) {
+    ListNode? slow = head;
+    ListNode? fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow?.next;
+        fast = fast!.next!.next;
+    }
+    return slow;
+  }
+}
