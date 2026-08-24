62a8f9e1626198d4c55b8a0299348c66eac0d5f0
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-24 21:22:32 +0300

    0083-remove-duplicates-from-sorted-list 08-24-2026 Easy .dart 1 ms (47.06%) 148.7 MB (62.74%)

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.dart
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? deleteDuplicates(ListNode? head) {
+    ListNode dummy = ListNode(0);
+    ListNode prev = dummy;
+    dummy.next = head;
+    while(head != null && head.next != null ){
+        if(head.val == head.next?.val){
+            prev.next = head.next;
+        }else{
+            prev = head;
+        }
+        head = head.next;
+        
+    }
+    return dummy.next;
+    
+  }
+}
