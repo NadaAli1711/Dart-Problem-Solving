85fc5785666f0bfac4c37ba0e7c4a2ee17f0b30c
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-25 15:18:15 +0300

    0234-palindrome-linked-list 08-25-2026 Easy .dart 3 ms (100%) 183.6 MB (65%)

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.dart
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  bool isPalindrome(ListNode? head) {
+    ListNode? slow = head;
+    ListNode? fast = head;
+    while(fast != null && fast.next != null){
+        slow = slow?.next;
+        fast = fast.next?.next;
+    }
+    ListNode? prev = null;
+    ListNode? current = slow;
+    // slow?.next = null;
+    while(current != null){
+        ListNode? temp = current.next;
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+    while(prev != null && head != null){
+        // print(head.val);
+        // print(prev.val);
+        if(prev.val != head.val)return false;
+        prev = prev.next;
+        head = head.next;
+    }
+
+    return true;
+    
+  }
+}
