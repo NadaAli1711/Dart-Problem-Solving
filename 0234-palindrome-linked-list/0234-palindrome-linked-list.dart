33f627726992faad5e690033962b4276a70be49c
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-25 12:18:32 +0000

    style: auto-format Dart code

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.dart
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.dart
@@ -10,28 +10,27 @@ class Solution {
   bool isPalindrome(ListNode? head) {
     ListNode? slow = head;
     ListNode? fast = head;
-    while(fast != null && fast.next != null){
-        slow = slow?.next;
-        fast = fast.next?.next;
+    while (fast != null && fast.next != null) {
+      slow = slow?.next;
+      fast = fast.next?.next;
     }
     ListNode? prev = null;
     ListNode? current = slow;
     // slow?.next = null;
-    while(current != null){
-        ListNode? temp = current.next;
-        current.next = prev;
-        prev = current;
-        current = temp;
+    while (current != null) {
+      ListNode? temp = current.next;
+      current.next = prev;
+      prev = current;
+      current = temp;
     }
-    while(prev != null && head != null){
-        // print(head.val);
-        // print(prev.val);
-        if(prev.val != head.val)return false;
-        prev = prev.next;
-        head = head.next;
+    while (prev != null && head != null) {
+      // print(head.val);
+      // print(prev.val);
+      if (prev.val != head.val) return false;
+      prev = prev.next;
+      head = head.next;
     }
 
     return true;
-    
   }
 }
