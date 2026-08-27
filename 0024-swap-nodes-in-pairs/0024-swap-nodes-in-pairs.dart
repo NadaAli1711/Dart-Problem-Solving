50c684410084f28f795672fcfc66d879df6f7a25
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-27 20:30:25 +0000

    style: auto-format Dart code

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.dart
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.dart
@@ -9,17 +9,16 @@
 class Solution {
   ListNode? swapPairs(ListNode? head) {
     ListNode dummy = ListNode(0);
-    ListNode prev = dummy; 
+    ListNode prev = dummy;
     prev.next = head;
-    while(head != null && head.next != null){
-        ListNode? temp = head.next;
-        head.next = temp?.next;
-        temp?.next = head;
-        prev.next = temp;
-        prev = head;
-        head = head.next;
+    while (head != null && head.next != null) {
+      ListNode? temp = head.next;
+      head.next = temp?.next;
+      temp?.next = head;
+      prev.next = temp;
+      prev = head;
+      head = head.next;
     }
     return dummy.next;
-    
   }
 }
