5881b31bea743eb2a8f6dc2cd2725839167ab25b
NadaAli1711 <nadaali2220222@gmail.com>
2026-08-29 13:08:50 +0300

    0002-add-two-numbers 08-29-2026 Medium .dart 1 ms (100%) 152.5 MB (24.49%)

---
+++ 0002-add-two-numbers/0002-add-two-numbers.dart
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    ListNode? dummy = ListNode(0);
+    ListNode? current = ListNode(0);
+    dummy = current;
+    int carry = 0;
+    while(l1 != null || l2 != null || carry != 0){
+        int digit = (l1?.val ?? 0) + (l2?.val ?? 0 ) + carry;
+        carry = digit ~/ 10;
+        digit %= 10;
+        current?.next = ListNode(digit);
+        current = current?.next;
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+    return dummy.next;
+    
+  }
+}
