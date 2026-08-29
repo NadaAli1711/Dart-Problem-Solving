df7d565f77bc11f37991669fcd2fa116f12ba41a
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-29 10:09:07 +0000

    style: auto-format Dart code

--- 0002-add-two-numbers/0002-add-two-numbers.dart
+++ 0002-add-two-numbers/0002-add-two-numbers.dart
@@ -12,16 +12,15 @@ class Solution {
     ListNode? current = ListNode(0);
     dummy = current;
     int carry = 0;
-    while(l1 != null || l2 != null || carry != 0){
-        int digit = (l1?.val ?? 0) + (l2?.val ?? 0 ) + carry;
-        carry = digit ~/ 10;
-        digit %= 10;
-        current?.next = ListNode(digit);
-        current = current?.next;
-        l1 = l1?.next;
-        l2 = l2?.next;
+    while (l1 != null || l2 != null || carry != 0) {
+      int digit = (l1?.val ?? 0) + (l2?.val ?? 0) + carry;
+      carry = digit ~/ 10;
+      digit %= 10;
+      current?.next = ListNode(digit);
+      current = current?.next;
+      l1 = l1?.next;
+      l2 = l2?.next;
     }
     return dummy.next;
-    
   }
 }
