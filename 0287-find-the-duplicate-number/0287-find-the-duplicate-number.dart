8dd6a99035bad622d43786925a0a3de3e6f33fde
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-08-31 13:22:18 +0000

    style: auto-format Dart code

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.dart
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.dart
@@ -2,16 +2,15 @@ class Solution {
   int findDuplicate(List<int> nums) {
     int slow = nums[0];
     int fast = nums[0];
-    do{
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    }while(slow != fast);
+    do {
+      slow = nums[slow];
+      fast = nums[nums[fast]];
+    } while (slow != fast);
     slow = nums[0];
-    while(slow != fast){
-        slow = nums[slow];
-        fast = nums[fast];
+    while (slow != fast) {
+      slow = nums[slow];
+      fast = nums[fast];
     }
     return slow;
-    
   }
 }
