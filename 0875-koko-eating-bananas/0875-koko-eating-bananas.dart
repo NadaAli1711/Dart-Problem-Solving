8badac6237231a57519feb2d42ccea1f83ab4773
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-02 23:05:53 +0300

    0875-koko-eating-bananas 09-02-2026 Medium .dart 8 ms (100%) 153.1 MB (58.82%)

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.dart
@@ -0,0 +1,20 @@
+class Solution {
+  int minEatingSpeed(List<int> piles, int h) {
+    int left = 1;
+    int right = piles.reduce(max);
+    while(left <= right){
+        int mid = (left + right) ~/ 2;
+        int totalHours = 0;
+        for(int i = 0 ; i < piles.length ; i++){
+            totalHours += (piles[i] + mid - 1) ~/ mid;
+        }
+        if(totalHours <= h){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+
+    }
+    return left;
+  }
+}
