6451e2674b360f3263077f251b44181e06b2a471
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-06 20:49:11 +0000

    style: auto-format Dart code

--- 0771-jewels-and-stones/0771-jewels-and-stones.dart
+++ 0771-jewels-and-stones/0771-jewels-and-stones.dart
@@ -1,8 +1,8 @@
 class Solution {
   int numJewelsInStones(String jewels, String stones) {
     int count = 0;
-    for(int i = 0 ; i < stones.length ; i++){
-        if(jewels.contains(stones[i])) count++;
+    for (int i = 0; i < stones.length; i++) {
+      if (jewels.contains(stones[i])) count++;
     }
     return count;
   }
