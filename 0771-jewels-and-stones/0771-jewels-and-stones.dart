a92ed8b0fc762792ffef59ab617a2238d483d40c
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-06 23:48:52 +0300

    0771-jewels-and-stones 09-06-2026 Easy .dart 0 ms (100%) 147.9 MB (61.54%)

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.dart
@@ -0,0 +1,9 @@
+class Solution {
+  int numJewelsInStones(String jewels, String stones) {
+    int count = 0;
+    for(int i = 0 ; i < stones.length ; i++){
+        if(jewels.contains(stones[i])) count++;
+    }
+    return count;
+  }
+}
