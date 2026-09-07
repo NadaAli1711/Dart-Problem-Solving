47d7535832f7b961343649cbed0aa71f96ff02d5
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-07 16:36:40 +0300

    0202-happy-number 09-07-2026 Easy .dart 2 ms (81.82%) 150.9 MB (15.15%)

---
+++ 0202-happy-number/0202-happy-number.dart
@@ -0,0 +1,18 @@
+class Solution {
+  bool isHappy(int n) {
+    Set<int> set = {};
+    int res = 0;
+    while(res != 1){
+        res = 0;
+        while(n != 0){
+            int temp = n % 10;
+            res += temp*temp;
+            n ~/= 10;
+        }
+    if(set.contains(res))return false;
+    set.add(res);
+    n = res;
+    }
+    return true;
+  }
+}
