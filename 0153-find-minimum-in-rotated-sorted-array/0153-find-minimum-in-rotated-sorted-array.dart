d47bd46c00dd6bc4b0ec32bc0fe21461a9a7bfef
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-08 18:43:32 +0000

    style: auto-format Dart code

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.dart
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.dart
@@ -2,14 +2,14 @@ class Solution {
   int findMin(List<int> nums) {
     int left = 0;
     int right = nums.length - 1;
-    while(left < right){
-        int mid = (left + right) ~/ 2;
-        if(nums[mid] > nums[right]){
-            left = mid + 1; 
-        }else{
-            right = mid;
-        }
+    while (left < right) {
+      int mid = (left + right) ~/ 2;
+      if (nums[mid] > nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
     return nums[left];
-      }
+  }
 }
