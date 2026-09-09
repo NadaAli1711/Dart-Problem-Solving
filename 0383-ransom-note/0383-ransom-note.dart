51af629d4958918189b87a322d19a65a54cff3ad
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-09 23:11:32 +0300

    0383-ransom-note 09-09-2026 Easy .dart 20 ms (31.25%) 153.3 MB (6.25%)

--- 0383-ransom-note/0383-ransom-note.dart
+++ 0383-ransom-note/0383-ransom-note.dart
@@ -1,12 +1,16 @@
 class Solution {
   bool canConstruct(String ransomNote, String magazine) {
-    Map<String, int> magazineMap = {};
-    for (int i = 0; i < magazine.length; i++) {
-      magazineMap[magazine[i]] = (magazineMap[magazine[i]] ?? 0) + 1;
+    if (ransomNote.length > magazine.length) return false;
+    Map<String,int> magazineMap = {};
+    for(int i = 0 ; i < magazine.length ; i++){
+        magazineMap[magazine[i]] = (magazineMap[magazine[i]] ?? 0) + 1;
     }
-    for (int i = 0; i < ransomNote.length; i++) {
-      if ((magazineMap[ransomNote[i]] ?? 0) == 0) return false;
-      magazineMap[ransomNote[i]] = (magazineMap[ransomNote[i]] ?? 0) - 1;
+    for(int i = 0 ; i < ransomNote.length ; i++){
+        final char = ransomNote[i];
+        final count = magazineMap[char] ?? 0;
+
+        if (count == 0) return false;
+        magazineMap[char] = count - 1;
     }
     return true;
   }
