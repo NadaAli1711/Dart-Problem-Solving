2518c159d94cf6ef666699f1938e24ac871f8ae4
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-09 20:09:09 +0000

    style: auto-format Dart code

--- 0383-ransom-note/0383-ransom-note.dart
+++ 0383-ransom-note/0383-ransom-note.dart
@@ -1,12 +1,12 @@
 class Solution {
   bool canConstruct(String ransomNote, String magazine) {
-    Map<String,int> magazineMap = {};
-    for(int i = 0 ; i < magazine.length ; i++){
-        magazineMap[magazine[i]] = (magazineMap[magazine[i]] ?? 0) + 1;
+    Map<String, int> magazineMap = {};
+    for (int i = 0; i < magazine.length; i++) {
+      magazineMap[magazine[i]] = (magazineMap[magazine[i]] ?? 0) + 1;
     }
-    for(int i = 0 ; i < ransomNote.length ; i++){
-        if((magazineMap[ransomNote[i]]??0) == 0) return false;
-         magazineMap[ransomNote[i]] = (magazineMap[ransomNote[i]] ?? 0) - 1;
+    for (int i = 0; i < ransomNote.length; i++) {
+      if ((magazineMap[ransomNote[i]] ?? 0) == 0) return false;
+      magazineMap[ransomNote[i]] = (magazineMap[ransomNote[i]] ?? 0) - 1;
     }
     return true;
   }
