8d69f2e0c08cb8fec13ff682b4e64c02d28063b4
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-13 19:32:58 +0000

    style: auto-format Dart code

--- 0100-same-tree/0100-same-tree.dart
+++ 0100-same-tree/0100-same-tree.dart
@@ -9,14 +9,13 @@
  */
 class Solution {
   bool isSameTree(TreeNode? p, TreeNode? q) {
-    if(p == null && q == null) {
-        return true;
-    }else if(p != null && q != null && p.val != q.val){
-        return false;
-    }else if((p != null && q == null) || p == null && q != null){
-        return false;
+    if (p == null && q == null) {
+      return true;
+    } else if (p != null && q != null && p.val != q.val) {
+      return false;
+    } else if ((p != null && q == null) || p == null && q != null) {
+      return false;
     }
-    return isSameTree(p?.right,q?.right) && isSameTree(p?.left,q?.left);
-    
+    return isSameTree(p?.right, q?.right) && isSameTree(p?.left, q?.left);
   }
 }
