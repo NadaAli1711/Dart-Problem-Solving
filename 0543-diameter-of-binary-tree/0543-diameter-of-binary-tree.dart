a506fa61f1b70df682a151497511e5ef5d8bf59c
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-14 20:13:00 +0300

    0543-diameter-of-binary-tree 09-14-2026 Easy .dart 1 ms (94.12%) 152.5 MB (23.53%)

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.dart
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+    late int max;
+    int diameterOfBinaryTree(TreeNode? root) {
+        max = 0;
+        helper(root);
+        return max;
+    }
+    int helper(TreeNode? root) {
+        if(root == null)return 0;
+        int leftHeight = helper(root.left);
+        int rightHeight = helper(root.right);
+        int temp = leftHeight + rightHeight;
+        if(max < temp) max = temp;
+        return 1 + ((leftHeight > rightHeight) ? leftHeight :rightHeight);
+        
+  }
+}
