1369f42dc8ba7e596c5b4234f472d0251638968c
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-15 18:03:32 +0000

    style: auto-format Dart code

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.dart
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.dart
@@ -1,14 +1,13 @@
 class Solution {
   int findClosestNumber(List<int> nums) {
     int closest = nums[0];
-    for(int i = 1 ; i < nums.length ; i++){
-        if(closest.abs() > nums[i].abs()){
-            closest = nums[i];
-        }else if(closest.abs() == nums[i].abs() && closest < nums[i]){
-            closest = nums[i];
-        }
+    for (int i = 1; i < nums.length; i++) {
+      if (closest.abs() > nums[i].abs()) {
+        closest = nums[i];
+      } else if (closest.abs() == nums[i].abs() && closest < nums[i]) {
+        closest = nums[i];
+      }
     }
     return closest;
-    
   }
 }
