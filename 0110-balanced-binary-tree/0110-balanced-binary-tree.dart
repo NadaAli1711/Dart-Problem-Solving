c277da53333df4ab922391c6ad0caa41a48a2f3f
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-17 22:23:58 +0300

    0110-balanced-binary-tree 09-17-2026 Easy .dart 2 ms (19.23%) 147.9 MB (88.46%)

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.dart
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool isBalanced(TreeNode? root) {
+    if(root == null) return true;
+    int leftHeight = getHeight(root.left);
+    int rightHeight = getHeight(root.right);
+    bool isCurrentBalanced = (leftHeight - rightHeight).abs() <= 1;
+    return isCurrentBalanced && isBalanced(root.left) && isBalanced(root.right);
+    
+  }
+  int getHeight(TreeNode? root){
+    if(root == null) return 0;
+    int leftPath = getHeight(root.left);
+    int rightPath = getHeight(root.right); 
+    return 1 + (leftPath > rightPath ? leftPath : rightPath);
+  }
+}
