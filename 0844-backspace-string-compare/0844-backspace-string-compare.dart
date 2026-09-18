ff1c033da3dd7e9c232c4a411f990fb103e22055
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-18 20:09:02 +0000

    style: auto-format Dart code

--- 0844-backspace-string-compare/0844-backspace-string-compare.dart
+++ 0844-backspace-string-compare/0844-backspace-string-compare.dart
@@ -1,23 +1,25 @@
 class Solution {
   bool backspaceCompare(String s, String t) {
-    if(s == t) return true;
+    if (s == t) return true;
     List<String> sStack = [];
     List<String> tStack = [];
-    for(int i = 0 ; i < s.length ; i++){
-        if(s[i] == '#'){
-            if(sStack.isNotEmpty){
-                sStack.removeLast();}
-        }else{
-            sStack.add(s[i]);
+    for (int i = 0; i < s.length; i++) {
+      if (s[i] == '#') {
+        if (sStack.isNotEmpty) {
+          sStack.removeLast();
         }
+      } else {
+        sStack.add(s[i]);
+      }
     }
-    for(int i = 0 ; i < t.length ; i++){
-        if(t[i] == '#'){
-            if(tStack.isNotEmpty){
-            tStack.removeLast();}
-        }else{
-            tStack.add(t[i]);
+    for (int i = 0; i < t.length; i++) {
+      if (t[i] == '#') {
+        if (tStack.isNotEmpty) {
+          tStack.removeLast();
         }
+      } else {
+        tStack.add(t[i]);
+      }
     }
     return sStack.join() == tStack.join();
   }
