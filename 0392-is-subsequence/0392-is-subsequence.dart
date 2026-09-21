2d9df244ec15e091976229a52e69cc81b8df3aa9
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-21 19:46:37 +0000

    style: auto-format Dart code

--- 0392-is-subsequence/0392-is-subsequence.dart
+++ 0392-is-subsequence/0392-is-subsequence.dart
@@ -1,14 +1,14 @@
 class Solution {
   bool isSubsequence(String s, String t) {
-    if(s == t || s.length == 0)return true;
+    if (s == t || s.length == 0) return true;
     int sPointer = 0;
     int tPointer = 0;
-    while(tPointer < t.length){
-        if(t[tPointer] == s[sPointer]){
-            sPointer++;
-            if(sPointer == s.length)return true;
-        }
-        tPointer++;
+    while (tPointer < t.length) {
+      if (t[tPointer] == s[sPointer]) {
+        sPointer++;
+        if (sPointer == s.length) return true;
+      }
+      tPointer++;
     }
     return false;
   }
