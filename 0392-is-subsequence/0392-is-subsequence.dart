38e323f0b4b79a684cbe8a3ed6eeabff8527cb15
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-21 22:46:19 +0300

    0392-is-subsequence 09-21-2026 Easy .dart 0 ms (100%) 152.5 MB (14.29%)

---
+++ 0392-is-subsequence/0392-is-subsequence.dart
@@ -0,0 +1,15 @@
+class Solution {
+  bool isSubsequence(String s, String t) {
+    if(s == t || s.length == 0)return true;
+    int sPointer = 0;
+    int tPointer = 0;
+    while(tPointer < t.length){
+        if(t[tPointer] == s[sPointer]){
+            sPointer++;
+            if(sPointer == s.length)return true;
+        }
+        tPointer++;
+    }
+    return false;
+  }
+}
