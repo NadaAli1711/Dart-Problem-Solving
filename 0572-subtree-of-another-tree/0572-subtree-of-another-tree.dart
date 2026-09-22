098d1c429493204860d1ccb927425fd562db4445
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-22 18:13:56 +0000

    style: auto-format Dart code

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.dart
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.dart
@@ -9,13 +9,16 @@
  */
 class Solution {
   bool isSubtree(TreeNode? root, TreeNode? subRoot) {
-    if(root == null)return false;
-    if(sameTree(root,subRoot)) return true;
-    return isSubtree(root.right,subRoot) || isSubtree(root.left,subRoot);
+    if (root == null) return false;
+    if (sameTree(root, subRoot)) return true;
+    return isSubtree(root.right, subRoot) || isSubtree(root.left, subRoot);
   }
-  bool sameTree(TreeNode? root, TreeNode? subRoot){
-    if(root == null && subRoot == null) return true;
-    if(root == null || subRoot == null || root.val != subRoot.val)return false;
-    return sameTree(root.right , subRoot.right) && sameTree(root.left , subRoot.left);
+
+  bool sameTree(TreeNode? root, TreeNode? subRoot) {
+    if (root == null && subRoot == null) return true;
+    if (root == null || subRoot == null || root.val != subRoot.val)
+      return false;
+    return sameTree(root.right, subRoot.right) &&
+        sameTree(root.left, subRoot.left);
   }
 }
