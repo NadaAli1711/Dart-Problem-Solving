ee9568ec429f2b3b8c05d927b5f23b59a4ced375
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-24 20:33:25 +0000

    style: auto-format Dart code

--- 0101-symmetric-tree/0101-symmetric-tree.dart
+++ 0101-symmetric-tree/0101-symmetric-tree.dart
@@ -9,11 +9,14 @@
  */
 class Solution {
   bool isSymmetric(TreeNode? root) {
-    return isSame(root?.left,root?.right);
+    return isSame(root?.left, root?.right);
   }
-  bool isSame(TreeNode? leftNode , TreeNode? rightNode){
-    if(leftNode == null && rightNode == null) return true;
-    if(leftNode == null || rightNode == null || leftNode.val != rightNode.val)return false;
-    return isSame(leftNode.left , rightNode.right) && isSame(leftNode.right , rightNode.left) ;
+
+  bool isSame(TreeNode? leftNode, TreeNode? rightNode) {
+    if (leftNode == null && rightNode == null) return true;
+    if (leftNode == null || rightNode == null || leftNode.val != rightNode.val)
+      return false;
+    return isSame(leftNode.left, rightNode.right) &&
+        isSame(leftNode.right, rightNode.left);
   }
 }
