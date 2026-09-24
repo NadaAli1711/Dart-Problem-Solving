e1f874fc2e558af3dc959e90cc7c098d15162464
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-24 23:33:06 +0300

    0101-symmetric-tree 09-24-2026 Easy .dart 0 ms (100%) 146.8 MB (100%)

---
+++ 0101-symmetric-tree/0101-symmetric-tree.dart
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *   int val;
+ *   TreeNode? left;
+ *   TreeNode? right;
+ *   TreeNode([this.val = 0, this.left, this.right]);
+ * }
+ */
+class Solution {
+  bool isSymmetric(TreeNode? root) {
+    return isSame(root?.left,root?.right);
+  }
+  bool isSame(TreeNode? leftNode , TreeNode? rightNode){
+    if(leftNode == null && rightNode == null) return true;
+    if(leftNode == null || rightNode == null || leftNode.val != rightNode.val)return false;
+    return isSame(leftNode.left , rightNode.right) && isSame(leftNode.right , rightNode.left) ;
+  }
+}
