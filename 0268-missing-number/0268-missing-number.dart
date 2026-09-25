e83c94ec528b6373fd1b5b532a700f2b1b6d60a2
NadaAli1711 <nadaali2220222@gmail.com>
2026-09-25 23:30:03 +0300

    0268-missing-number 09-25-2026 Easy .dart 0 ms (100%) 147.4 MB (100%)

---
+++ 0268-missing-number/0268-missing-number.dart
@@ -0,0 +1,16 @@
+class Solution {
+  int missingNumber(List<int> nums) {
+    int temp = 0;
+    int max = nums.length;
+    for(int i = 0 ; i < nums.length ; i++){
+        temp ^= nums[i];
+    }
+        for(int i = 1 ; i <= nums.length ; i++){
+        temp ^= i;
+    }
+    return temp;
+
+
+    
+  }
+}
