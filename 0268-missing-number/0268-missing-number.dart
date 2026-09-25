177e2cd4631531051c2bdd827823d7416c8331d1
NadaAli1711 <NadaAli1711@users.noreply.github.com>
2026-09-25 20:30:22 +0000

    style: auto-format Dart code

--- 0268-missing-number/0268-missing-number.dart
+++ 0268-missing-number/0268-missing-number.dart
@@ -2,15 +2,12 @@ class Solution {
   int missingNumber(List<int> nums) {
     int temp = 0;
     int max = nums.length;
-    for(int i = 0 ; i < nums.length ; i++){
-        temp ^= nums[i];
+    for (int i = 0; i < nums.length; i++) {
+      temp ^= nums[i];
     }
-        for(int i = 1 ; i <= nums.length ; i++){
-        temp ^= i;
+    for (int i = 1; i <= nums.length; i++) {
+      temp ^= i;
     }
     return temp;
-
-
-    
   }
 }
